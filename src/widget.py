aaad11d9058151d4620f6135198f9276d0d00aeb
Daniil Tarasov <danya.tarasov.01@mail.ru>
2024-09-20 15:36:15 +0300

    new module

---
+++ src/widget.py
@@ -0,0 +1,18 @@
+def mask_account_card(card_or_account: str) -> str:
+    """Возвращает строку с замаскированным номером"""
+    card_or_account_number = ""
+    for numbers in card_or_account:
+        if numbers.isdigit():
+            card_or_account_number += numbers
+    if len(card_or_account_number) == 16:
+        return (f"{
+        card_or_account_number[:4]} "
+                f"{card_or_account_number[4:6]}** "
+                f"**** {card_or_account_number[-4:]}")
+    else:
+        return "**" + card_or_account_number[-4:]
+
+
+if __name__ == '__main__':
+    card_or_account = input()
+    print(mask_account_card(card_or_account))
