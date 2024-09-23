639a52294e04f403338d3f7bf67cf6baf8c0138e
Daniil Tarasov <danya.tarasov.01@mail.ru>
2024-09-23 12:22:53 +0300

    added type of operation

--- src/widget.py
+++ src/widget.py
@@ -4,13 +4,16 @@ from masks import get_mask_account, get_mask_card_number
 def mask_account_card(card_or_account: str) -> str:
     """Возвращает строку с замаскированным номером"""
     card_or_account_number = ""
-    for numbers in card_or_account:
-        if numbers.isdigit():
-            card_or_account_number += numbers
+    type_of_operation = ""
+    for i in card_or_account:
+        if i.isdigit():
+            card_or_account_number += i
+        else:
+            type_of_operation += i
     if len(card_or_account_number) == 16:
-        return get_mask_card_number(card_or_account_number)
+        return f"{type_of_operation}{get_mask_card_number(card_or_account_number)}"
     else:
-        return get_mask_account(card_or_account_number)
+        return f"{type_of_operation}{get_mask_account(card_or_account_number)}"
 
 
 def get_date(full_date: str) -> str:
