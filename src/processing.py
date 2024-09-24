91c031f35c8106701b0010648acf7b03763fe5a4
Daniil Tarasov <danya.tarasov.01@mail.ru>
2024-09-25 00:53:11 +0300

    update moduls and README

--- src/processing.py
+++ src/processing.py
@@ -1,18 +1,19 @@
 from typing import Iterable
 
-def filter_by_state(info_about_operation: Iterable[list], state: str="EXECUTED") -> list:
+
+def filter_by_state(info_about_operation: Iterable[list], state: str = "EXECUTED", /) -> list:
     """Возвращает новый список по заданному ключу"""
     new_list = []
     for key in info_about_operation:
-        if key.get("state") == state:
+        if key["state"] == state:
             new_list.append(key)
     return new_list
 
 
-def sort_by_date(info_about_operation: Iterable[list], ascending: bool=True) -> list:
+def sort_by_date(info_about_operation: Iterable[list], ascending: bool = True, /) -> list:
     """Возвращает отсортированный список по дате"""
-    if ascending == True:
-        new_sorted_list = sorted(info_about_operation, key=lambda date: date.get("date"), reverse=True)
+    if ascending is True:
+        new_sorted_list = sorted(info_about_operation, key=lambda date: date["date"], reverse=True)
     else:
-        new_sorted_list = sorted(info_about_operation, key=lambda date: date.get("date"))
+        new_sorted_list = sorted(info_about_operation, key=lambda date: date["date"])
     return new_sorted_list
