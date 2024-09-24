80c1a53b1995c7a006a5f501695c69bafec85c7b
Daniil Tarasov <danya.tarasov.01@mail.ru>
2024-09-24 23:27:09 +0300

    new function in processing

--- src/processing.py
+++ src/processing.py
@@ -1,9 +1,18 @@
 from typing import Iterable
 
-def filter_by_state(list_dict: Iterable[list], state: str="EXECUTED") -> list:
+def filter_by_state(info_about_operation: Iterable[list], state: str="EXECUTED") -> list:
     """Возвращает новый список по заданному ключу"""
     new_list = []
-    for key in list_dict:
+    for key in info_about_operation:
         if key.get("state") == state:
             new_list.append(key)
     return new_list
+
+
+def sort_by_date(info_about_operation: Iterable[list], ascending: bool=True) -> list:
+    """Возвращает отсортированный список по дате"""
+    if ascending == True:
+        new_sorted_list = sorted(info_about_operation, key=lambda date: date.get("date"), reverse=True)
+    else:
+        new_sorted_list = sorted(info_about_operation, key=lambda date: date.get("date"))
+    return new_sorted_list
