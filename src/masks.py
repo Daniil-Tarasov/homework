68eff21f735da4fe743f01c54801d37af7ed27c1
Daniil Tarasov <danya.tarasov.01@mail.ru>
2024-10-20 20:07:11 +0300

    add log in masks and utils

--- src/masks.py
+++ src/masks.py
@@ -1,8 +1,28 @@
+import logging
+
+logger = logging.getLogger("masks")
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler("../logs/masks.log", "w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s - %(filename)s %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+
+
 def get_mask_card_number(card_number: str) -> str:
     """Маскирует номер банковской карты"""
-    return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+    logger.info(f"Внесённые данные {card_number}")
+    try:
+        return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+    except TypeError as ex:
+        logger.error(f"Произошла ошибка {ex}")
+        return "Передана не строка"
 
 
 def get_mask_account(account_number: str) -> str:
     """Маскирует номер банковского счёта"""
-    return "**" + account_number[-4:]
+    logger.info(f"Внесённые данные {account_number}")
+    try:
+        return "**" + account_number[-4:]
+    except TypeError as ex:
+        logger.error(f"Произошла ошибка {ex}")
+        return "Передана не строка"
