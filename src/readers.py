2dafeb2db34c8759331ae7dea9460a592f5fe4da
Daniil Tarasov <danya.tarasov.01@mail.ru>
2024-10-25 17:16:21 +0300

    add new module

---
+++ src/readers.py
@@ -0,0 +1,45 @@
+import pandas as pd
+
+
+def get_data_from_csv(path_to_the_file: str) -> list:
+    """Функция, которая возвращает данные о финансовых транзакциях из файла csv"""
+    try:
+        with open(path_to_the_file, encoding="utf-8") as file:
+            pd.read_csv(file)
+
+    except ValueError:
+
+        return []
+
+    except FileNotFoundError:
+
+        return []
+
+    else:
+
+        with open(path_to_the_file, encoding="utf-8") as file:
+            operations = pd.read_csv(file, delimiter=';')
+            return operations.to_dict(orient='records')
+
+
+def get_data_from_excel(path_to_the_file: str) -> list:
+    """Функция, которая возвращает данные о финансовых транзакциях из файла excel"""
+    try:
+        with open(path_to_the_file, encoding="utf-8") as file:
+            pd.read_excel(file)
+
+#    except ValueError:
+
+#        return []
+
+    except FileNotFoundError:
+
+        return []
+
+    else:
+
+        with open(path_to_the_file, encoding="utf-8") as file:
+            operations = pd.read_excel(file)
+            return operations.to_dict(orient='records')
+
+print(get_data_from_excel('../data/transactions_excel.xlsx'))
